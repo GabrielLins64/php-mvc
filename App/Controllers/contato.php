f870601d9ac2609dcefd370bb7f8304eb2d63b88
Gabriel Lins <gabrielinsmelo@gmail.com>
2021-11-11 19:19:19 -0300

    feat: add routing system and basic controllers

---
+++ App/Controllers/contato.php
@@ -0,0 +1,21 @@
+<?php
+
+class Contato
+{
+  public function index()
+  {
+    echo "Index do contato";
+  }
+  
+  public function email()
+  {
+    echo "meuemail@email";
+  }
+
+  public function telefone()
+  {
+    echo "12345679";
+  }
+}
+
+?>
