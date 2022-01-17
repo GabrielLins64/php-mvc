7358b27b4d2a0ccce59eb803319613c6bc5409cb
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-01-17 17:25:10 -0300

    fix: notfound page button

--- App/Views/global/notfound.php
+++ App/Views/global/notfound.php
@@ -1,7 +1,7 @@
 
+<input type="button" onclick="location.href='/'" value="Voltar">
 <h1>404 - Página não encontrada!</h1>
 
 <hr>
 
 <h3>A página que você procura não está aqui!</h3>
-<a href="/">Voltar</a>
