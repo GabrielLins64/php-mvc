f18ccf788ef74ca4168fee3ef872bf595ce68c55
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-02-06 15:05:05 -0300

    fix: image upload error flow

--- App/Controllers/notes.php
+++ App/Controllers/notes.php
@@ -6,6 +6,8 @@ use \App\Core\Controller;
 
 class Notes extends Controller
 {
+  private Array $uploadErrors;
+
   public function index()
   {
     $note = $this->model('Note');
@@ -25,58 +27,27 @@ class Notes extends Controller
   public function criar()
   {
     Auth::CheckLogin();
-    
+
     $mensagem = array();
-    
+
     if (isset($_POST['cadastrar'])):
       if (empty($_POST['titulo']) || empty($_POST['texto'])):
         $mensagem[] = "Todos os campos devem ser preenchidos.";
       else:
-        $storage = new \Upload\Storage\FileSystem('./assets/uploads');
-        $file = new \Upload\File('noteImage', $storage);
-
-        // Optionally you can rename the file on upload
-        $new_filename = uniqid();
-        $file->setName($new_filename);
-
-        // Validate file upload
-        $file->addValidations(array(
-          // Ensure file is of type "image/png"
-          new \Upload\Validation\Mimetype('image/png'),
-
-          //You can also add multi mimetype validation
-          //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
-
-          // Ensure file is no larger than 5M (use "B", "K", M", or "G")
-          new \Upload\Validation\Size('5M')
-        ));
-
-        // Access data about the file that has been uploaded
-        $data = array(
-          'name'       => $file->getNameWithExtension(),
-          'extension'  => $file->getExtension(),
-          'mime'       => $file->getMimetype(),
-          'size'       => $file->getSize(),
-          'md5'        => $file->getMd5(),
-          'dimensions' => $file->getDimensions()
-        );
-
-        // Try to upload file
-        try {
-          // Success!
-          $file->upload();
-          $mensagem[] = "Upload feito com sucesso!";
-
-          $note = $this->model('Note');
-          $note->title = $_POST['titulo'];
-          $note->text = $_POST['texto'];
-          $mensagem[] = $note->save();
+        if ($_FILES['noteImage']['size'] != 0) {
+          try {
+            $this->uploadImage();
+          } catch (\Exception $e) {
+            $mensagem[] = implode("<br>", $this->uploadErrors);
+            $this->view('notes/criar', $dados = ['mensagem' => $mensagem]);
+            return;
+          }
         }
-        catch (\Exception $e) {
-          // Fail!
-          $errors = $file->getErrors();
-          $mensagem[] = implode("<br>", $errors);
-        }
+
+        $note = $this->model('Note');
+        $note->title = $_POST['titulo'];
+        $note->text = $_POST['texto'];
+        $mensagem[] = $note->save();
       endif;
     endif;
 
@@ -115,6 +86,46 @@ class Notes extends Controller
     $dados = $note->getAll();
 
     $this->view('home/index', $dados = ['registros' => $dados, 'mensagem' => $mensagem]);
+  }
+
+  private function uploadImage()
+  {
+    $storage = new \Upload\Storage\FileSystem('./assets/uploads');
+    $file = new \Upload\File('noteImage', $storage);
+
+    // Optionally you can rename the file on upload
+    $new_filename = uniqid();
+    $file->setName($new_filename);
+
+    // Validate file upload
+    $file->addValidations(array(
+      // Ensure file is of type "image/png"
+      new \Upload\Validation\Mimetype('image/png'),
+
+      //You can also add multi mimetype validation
+      //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
+
+      // Ensure file is no larger than 5M (use "B", "K", M", or "G")
+      new \Upload\Validation\Size('5M')
+    ));
+
+    try {
+      // Access data about the file that has been uploaded
+      $data = array(
+        'name'       => $file->getNameWithExtension(),
+        'extension'  => $file->getExtension(),
+        'mime'       => $file->getMimetype(),
+        'size'       => $file->getSize(),
+        'md5'        => $file->getMd5(),
+        'dimensions' => $file->getDimensions()
+      );
+
+      $file->upload();
+    }
+    catch (\Exception $e) {
+      $this->uploadErrors = $file->getErrors();
+      throw $e;
+    }
   }
 }
 
--- App/Views/notes/criar.php
+++ App/Views/notes/criar.php
@@ -14,7 +14,7 @@
 <form action="/notes/criar" method="POST" enctype="multipart/form-data">
   <input placeholder="Título" type="text" name="titulo"><br>
   <textarea placeholder="Texto" name="texto"></textarea><br>
-  <button  type="button" onclick="document.getElementById('fileInput').click()">Imagem</button>
+  <button type="button" onclick="document.getElementById('fileInput').click()">Imagem</button>
   <label for="fileInput" id="file-label">Insira uma imagem</label><br>
   <input id="fileInput" type='file' name="noteImage" hidden>
   <button type="submit" name="cadastrar">Cadastrar</button>
