cf43965f6f4f7985cd5b127ccb85717aaab052e4
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-02-06 16:43:54 -0300

    feat: improve note view styling

--- App/Views/notes/ver.php
+++ App/Views/notes/ver.php
@@ -3,9 +3,14 @@
 
 <hr>
 
-<h3>Título: <?php echo $data['titulo']; ?></h3>
-<?php
-  if ($data['imagem'])
-    print "<img src='".URL_BASE."/assets/uploads/".$data['imagem']."' alt='Note Image' width='200'>";
-?>
-<p>Texto: <?php echo $data['texto']; ?></p><br>
+<div class="note-container">
+  <?php
+    if ($data['imagem']) {
+      print "<img src='".URL_BASE."/assets/uploads/".$data['imagem']."' alt='Note Image' class='note-img'>";
+    }
+  ?>
+  <div class="note-content">
+    <h3>Título: <?php echo $data['titulo']; ?></h3>
+    <p>Texto: <?php echo $data['texto']; ?></p><br>
+  </div>
+</div>
