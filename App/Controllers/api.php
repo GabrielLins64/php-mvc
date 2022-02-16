6bb16cf5e7d2348b1c861e24dc976d0d87d6184c
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-02-16 00:19:38 -0300

    fix: performance issue

--- App/Controllers/api.php
+++ App/Controllers/api.php
@@ -7,17 +7,17 @@ class Api extends Controller
 {
   public function notes()
   {
-    $note = $this->model('Note');
-    $dados = $note->getAll();
-
     if (   isset($_POST['email'])
        and isset($_POST['password'])
        and Auth::LoginApi($_POST['email'], $_POST['password']))
     {
+      $note = $this->model('Note');
+      $dados = $note->getAll();
       header('Content-Type: application/json; charset:utf-8');
       echo json_encode($dados);
     }
-    else {
+    else
+    {
       header('HTTP/1.1 401 Unauthorized; Content-Type: text/html; charset:utf-8');
       echo "<h1>401 Unauthorized</h1>";
       echo "Invalid credentials!";
